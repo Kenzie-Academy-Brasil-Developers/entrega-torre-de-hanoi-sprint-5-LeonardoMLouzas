5c4bbebb7a9a0cbc0be6ac9cb8f8a7380befbac8
leomlouzas <leonardomlouzas@gmail.com>
2021-09-28 12:43:38 -0300

    Adiciona os blocos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ for (let i = 0; i < 3; i++){
     body.appendChild(torres);
     torres.classList.add("torre"+(i+1));
 }
+
+for (let i = 1; i < 4; i++){
+    
+    const a = document.getElementsByClassName("torre1");
+    const b = document.createElement("div");
+    b.classList.add("bloco");
+    b.setAttribute("id","bloco"+i);
+    a[0].appendChild(b);
+}
