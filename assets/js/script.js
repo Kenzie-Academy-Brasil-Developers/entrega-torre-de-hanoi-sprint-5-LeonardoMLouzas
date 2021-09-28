48995dd6d9331357fbdd3b5158ff7cb6d120baf3
leomlouzas <leonardomlouzas@gmail.com>
2021-09-28 14:07:41 -0300

    Adiciona função para gerar os blocos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
+//Gera as torres
 for (let i = 0; i < 3; i++){
     const body = document.querySelector("section");
     const torres = document.createElement("div");
     body.appendChild(torres);
-    torres.classList.add("torre"+(i+1));
+    torres.classList.add("torre","torre"+(i+1));
 }
 
-for (let i = 1; i < 4; i++){
-    
-    const a = document.getElementsByClassName("torre1");
-    const b = document.createElement("div");
-    b.classList.add("bloco");
-    b.setAttribute("id","bloco"+i);
-    a[0].appendChild(b);
+//Controle dos botões para gerar os Blocos
+const botoes = document.querySelector(".controls");
+botoes.addEventListener("click", interceptar);
+
+function interceptar(evt){
+    const button = evt.target;
+    let maxBlocks = 0;
+
+    if (button.id == "playwith3") {
+        maxBlocks = 3;
+    }
+    if (button.id == "playwith4") {
+        maxBlocks = 4;
+    }
+    if (button.id == "playwith5") {
+        maxBlocks = 5;
+    }
+
+    //Gera os blocos
+    for (maxBlocks;maxBlocks > 0; maxBlocks--){
+        const torre = document.getElementsByClassName("torre1");
+        const bloco = document.createElement("div");
+        bloco.classList.add("bloco");
+        bloco.setAttribute("id","bloco"+ maxBlocks);
+        torre[0].appendChild(bloco);
+    }
 }
